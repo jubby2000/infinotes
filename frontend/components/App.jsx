9eadd0de78c19a6675fd8a8373688231231ba029
jacob barlow <jubby2000@gmail.com>
2018-06-05 11:46:56 -0700

    Propogate rename changes

--- frontend/components/App.jsx
+++ frontend/components/App.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 
 const App = () => (
   <div>
-    <h1>Infinote</h1>
+    <h1>Infinotes</h1>
     {/* <GreetingContainer /> */}
   </div>
 );
--- webpack.config.js
+++ webpack.config.js
@@ -2,7 +2,7 @@ const path = require('path');
 
 module.exports = {
   context: __dirname,
-  entry: './frontend/infinote.jsx',
+  entry: './frontend/infinotes.jsx',
   output: {
     path: path.resolve(__dirname, 'app', 'assets', 'javascripts'),
     filename: 'bundle.js'
