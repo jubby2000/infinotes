c476a9f03c4bb2f6fcea016c4123d88cf7f6b995
jacob barlow <jubby2000@gmail.com>
2018-06-05 12:35:02 -0700

    Modify configuration for Heroku

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,7 +4,6 @@ class ApplicationController < ActionController::Base
   helper_method :current_user, :require_login
 
   def current_user
-    # debugger
     @current_user ||= User.find_by_session_token(session[:session_token])
   end
 
