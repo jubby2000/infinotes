bcb574dc3b32bf4a4d9d39a0b61f312bbc5536f1
jacob barlow <jubby2000@gmail.com>
2018-06-10 19:47:25 -0700

    Modify migrations

---
+++ db/migrate/20180611023922_create_notebooks.rb
@@ -0,0 +1,12 @@
+class CreateNotebooks < ActiveRecord::Migration[5.1]
+  def change
+    drop_table :notebooks
+    create_table :notebooks do |t|
+      t.string :title, null: false, unique: true
+      t.integer :user_id
+
+      t.timestamps
+    end
+    add_index :notebooks, :user_id
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180607200541) do
+ActiveRecord::Schema.define(version: 20180611023922) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
